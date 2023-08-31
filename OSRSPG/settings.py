8c26761e377dcd1b86debcee2e5bbf752956e4bb
MooNight009 <aamajedi@gmail.com>
2023-08-31 15:36:29 +0200

    - Updated database handling for EB

--- OSRSPG/settings.py
+++ OSRSPG/settings.py
@@ -89,16 +89,28 @@ WSGI_APPLICATION = 'OSRSPG.wsgi.application'
 #     }
 # }
 # PostgreSQL database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '***REMOVED***',
-        'USER': 'postgres',
-        'PASSWORD': '***REMOVED***',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': '***REMOVED***',
+            'USER': 'postgres',
+            'PASSWORD': '***REMOVED***',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
