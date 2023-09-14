57f1462dafef8413caff34ce7dd9d3b5834f3356
MooNight009 <aamajedi@gmail.com>
2023-09-14 19:18:35 +0200

    - Added admin site for models

--- applications/submission/admin.py
+++ applications/submission/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from applications.submission.models import Submission
+
+admin.site.register(Submission)
--- applications/tile/admin.py
+++ applications/tile/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from applications.tile.models import Tile, TeamTile
+
+admin.site.register(Tile)
+admin.site.register(TeamTile)
