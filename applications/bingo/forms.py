418d9108b1b68c32e5026e47fd9483c0814f6ebc
MooNight009 <aamajedi@gmail.com>
2023-09-14 14:02:21 +0200

    - Added required to

--- applications/bingo/forms.py
+++ applications/bingo/forms.py
@@ -11,13 +11,19 @@ class BingoForm(forms.ModelForm):
         fields = ['name', 'description', 'start_date', 'end_date', 'board_type', 'board_size', 'is_public',
                   'is_team_public', 'can_players_create_team', 'max_players_in_team', 'img']
         widgets = {
-            'start_date': forms.SelectDateWidget(),
-            'end_date': forms.SelectDateWidget(),
-            'description': forms.Textarea(attrs={'class': 'form-control'})
+            'img': forms.FileInput(attrs={'class': 'form-control', 'required':'required'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'start_date': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'end_date': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'is_game_over_on_finish': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'is_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'is_team_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'max_players_in_team': forms.NumberInput(attrs={'class': 'form-control'})
         }
 
     def clean(self):
-        clean = super(EditBingoForm, self).clean()
+        clean = super().clean()
 
         # Make sure the new start date is not before today
         if clean['start_date'] <= datetime.datetime.now(datetime.timezone.utc):
