f58b76eace273afe90df7994f7071f0c6d8ac8c6
MooNight009 <aamajedi@gmail.com>
2023-09-18 16:25:09 +0200

    - Added password reset

--- applications/player/urls.py
+++ applications/player/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from applications.player.views import *
 
-app_name= 'player'
+app_name = 'player'
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('register', RegisterView.as_view(), name='register'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('account', AccountView.as_view(), name='account'),
-    path('activate/<uidb64>/<token>', ActivateView.as_view(), name='activate')
+    path('forgot_password', ForgotPasswordView.as_view(), name='forgot_password'),
+    path('activate/<uidb64>/<token>', ActivateView.as_view(), name='activate'),
+    path('reset_password/<uidb64>/<token>', ResetPasswordView.as_view(), name='reset_password'),
 ]
--- applications/player/views.py
+++ applications/player/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
@@ -8,7 +9,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views.generic import FormView, RedirectView, TemplateView
 
-from applications.player.forms import LoginForm, CreateUserForm
+from applications.player.forms import LoginForm, CreateUserForm, ForgotPasswordForm, ResetPasswordForm
 from applications.player.tokens import account_activation_token
 
 
@@ -16,11 +17,6 @@ class LoginView(FormView):
     template_name = 'pages/player/login.html'
     form_class = LoginForm
     success_url = '/'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # print(context)
-        return context
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -39,13 +35,6 @@ class RegisterView(FormView):
 
     def form_valid(self, form):
         user = form.save()
-        # user.is_active = False
-        # user.save
-
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password1']
-
-        # user = authenticate(self.request, username=username, password=password)
         if user is not None:
             # Send confirmation email
             current_site = get_current_site(self.request)
@@ -76,13 +65,60 @@ class LogoutView(RedirectView):
 class AccountView(TemplateView):
     template_name = 'pages/player/account.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(AccountView, self).get_context_data(**kwargs)
 
-        return context
+class ForgotPasswordView(FormView):
+    template_name = 'pages/player/forgot_password.html'
+    form_class = ForgotPasswordForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        user = User.objects.filter(email=form.cleaned_data['email']).first()
+
+        # Send password reset email
+        current_site = get_current_site(self.request)
+        mail_subject = 'Password change in OldSchool Bingo'
+        message = render_to_string('layouts/mails/password_reset.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        send_mail(
+            mail_subject,
+            message,
+            'info@oldschoolbingo.com',
+            [form.cleaned_data['email']]
+        )
+
+        return super().form_valid(form)
+
+
+class ResetPasswordView(FormView):
+    template_name = 'pages/player/reset_password.html'
+    form_class = ResetPasswordForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        try:
+            uid = force_str(urlsafe_base64_decode(self.kwargs['uidb64']))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, self.kwargs['token']):
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(self.request, user)
+
+        return super().form_valid(form)
 
 
 class ActivateView(RedirectView):
+    """
+        Activates the player's account
+        TODO: Switch to features requiring account activation
+    """
+
     def get_redirect_url(self, *args, **kwargs):
         try:
             uid = force_str(urlsafe_base64_decode(kwargs['uidb64']))
