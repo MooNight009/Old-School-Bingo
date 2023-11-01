30ed039477a66219287fa170019b482e830967b9
MooNight009 <aamajedi@gmail.com>
2023-11-01 13:37:37 +0100

    - Added localization to times when creating and editting bingo

--- applications/common/widgets.py
+++ applications/common/widgets.py
@@ -2,14 +2,13 @@ import datetime
 
 from django.forms import Widget
 from django.forms.widgets import Input
+from django.utils import timezone
 
 
 class DateTimeWidget(Input):
     input_type = 'datetime-local'
 
     def get_context(self, name, value, attrs):
-        attrs['min'] = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%dT%H:%M')
-        # attrs['value'] = '2018-06-12T19:30'
-        # attrs['max'] = '2018-06-12T19:30'
+        attrs['min'] = timezone.now().strftime('%Y-%m-%dT%H:%M')
         context = super().get_context(name, value, attrs)
         return context
